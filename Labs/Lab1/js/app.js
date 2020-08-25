8f747d903582417f7e6d504badffc2ff6e9b1597
bmbrian <49212185+bmbrian@users.noreply.github.com>
2020-08-25 19:18:38 -0400

    updates

---
+++ Labs/Lab1/js/app.js
@@ -0,0 +1,28 @@
+function setup() {
+    createCanvas(800,600);
+    strokeWeight(6);
+     line(200, 50, 195, 210);
+     
+     fill(0,0,0);
+     strokeWeight(6);
+     ellipse(200,50,150,70);
+    
+     stroke(156,6,9);
+    line(250, 200, 50, 50);
+    
+    stroke(156,6,9);
+    line(250, 150, 300, 50);
+   
+
+    fill(240,188,0);
+    circle(200,50,20);
+
+    fill(7,236,240);
+    circle(200,50,10);
+    fill(133,147,255);
+    rect(151,120,100,100);
+    fill(58,6,156);
+    triangle(30, 75, 58, 20, 86, 75);
+    
+
+}
