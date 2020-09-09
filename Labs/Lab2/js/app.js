fbe8efe27d623d1ce64250787f85dcc79bd4e84d
bmbrian <49212185+bmbrian@users.noreply.github.com>
2020-09-09 14:58:46 -0400

    Update app.js

--- Labs/Lab2/js/app.js
+++ Labs/Lab2/js/app.js
@@ -1,20 +1,15 @@
 function setup() {
     createCanvas(400, 300);
-    }
-    
-    var diameter = 20;
-    
-    function draw() {
-    background("yellow");
+   
       
     if(mouseX>200)
     {
     fill("red");
-    circle(mouseX,mouseY,diameter);
+    circle(mouseX,mouseY,30);
     }
     else
     {
     fill("blue");
-    circle(mouseX,mouseY,diameter);
+    circle(mouseX,mouseY,30);
     }
-    }
+    } 
