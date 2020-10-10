1456b997fc807694241104d2fbba464d84ec3c0b
bmbrian <49212185+bmbrian@users.noreply.github.com>
2020-10-09 21:08:12 -0400

    new

---
+++ Labs/Lab6AnimatedObject/js/app.js
@@ -0,0 +1,17 @@
+let x=500;
+let y=500;
+let size=300;
+function setup()
+{
+createCanvas(1000, 1000);
+}
+
+
+function draw(){
+
+background(0,0,0);
+let clr = color(253, 132, 53);
+fill(clr);  
+circle(x,y,size);
+
+}
