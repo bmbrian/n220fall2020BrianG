03d4e66ca66ef3a4b87bd77cd4b880ae2c842d15
bmbrian <49212185+bmbrian@users.noreply.github.com>
2020-10-30 18:30:53 -0400

    new

---
+++ Labs/Lab7Greeter/js/app.js
@@ -0,0 +1,19 @@
+function greet()
+
+{
+    var name=document.getElementById("textboxName").value;
+
+    
+
+    console.log(displayName(name));
+
+}
+
+
+function displayName(name)
+
+{
+
+    return "Hello "+name;
+
+}
