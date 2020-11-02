73ee67e87edf1525a028dfdfc042f270d916b64a
bmbrian <49212185+bmbrian@users.noreply.github.com>
2020-11-01 20:19:43 -0500

    new

---
+++ Labs/Lab7Numberguesser/js/app.js
@@ -0,0 +1,30 @@
+function onsubmitclick() {
+    if (document.getElementById("username").value == "Username" && document.getElementById("password").value == "Password") {
+        document.getElementById("result").innerHTML = "Success";
+    } else {
+        document.getElementById("result").innerHTML = "Wrong Information";
+    }
+
+}
+
+
+var y = Math.floor(Math.random() * 10 + 1); //here random number from 1 to 10 is generated
+var guess = 1; //initilize value start with guess 1 with start
+document.getElementById("submitguess").onclick = function(){
+var x = document.getElementById("guessField").value;
+if(x == y)
+{     
+alert("Suceessfully Guess!!! YOU GUESSED IT RIGHT IN "+ guess + " GUESS ");
+}
+else if(x > y) /* if guessed number is greater
+            than actual number*/
+{     
+guess++;
+alert("Entered Number is smaller then You Entered");
+}
+else
+{
+guess++;
+alert("Enter Number is Greater then You entered")
+}
+}
